d6b7c7d8e4f1e926158449d8e32d1a17c0a57be3
Barry K Suridge <barry.suridge@iinet.net.au>
2017-05-01 17:50:50 +1000

    Update test.py

--- MyDNS/test.py
+++ MyDNS/test.py
@@ -6,10 +6,14 @@
 ###
 
 from supybot.test import *
+import supybot.conf as conf
 
 
 class MyDNSTestCase(PluginTestCase):
     plugins = ('MyDNS',)
 
-
+    def testSimple(self):
+        self.assertNotError('mydns dns google.com')
+        self.assertNotError('mydns dns 2a03:2880:f119:8083:face:b00c:0:25de')
+        
 # vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
