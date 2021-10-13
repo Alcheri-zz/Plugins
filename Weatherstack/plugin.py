fdfd67c554778fe4e08c9a00ec9b27970993012d
Barry Suridge <Alcheri@users.noreply.github.com>
2021-10-13 19:48:45 +1100

    Update plugin.py

--- Weatherstack/plugin.py
+++ Weatherstack/plugin.py
@@ -31,11 +31,16 @@ import math
 import re
 import requests
 
+try:
+    import ephem
+except ImportError as ie:
+    raise Exception(f'Cannot import module: {ie}')
+
 from datetime import datetime
 from functools import lru_cache
 from requests.exceptions import HTTPError
 
-from supybot import callbacks, ircutils
+from supybot import callbacks, conf, ircutils
 from supybot.commands import *
 
 try:
@@ -44,39 +49,51 @@ except ImportError as ie:
     raise Exception(f"Cannot import module: {ie}")
 
 #XXX Unicode symbol (https://en.wikipedia.org/wiki/List_of_Unicode_characters#Latin-1_Supplement)
-apostrophe = "\N{APOSTROPHE}"
-degree_sign = "\N{DEGREE SIGN}"
+apostrophe     = u'\N{APOSTROPHE}'
+degree_sign    = u'\N{DEGREE SIGN}'
 #XXX micro_sign     = u'\N{MICRO SIGN}'
-percent_sign = "\N{PERCENT SIGN}"
-quotation_mark = "\N{QUOTATION MARK}"
-
+percent_sign   = u'\N{PERCENT SIGN}'
+quotation_mark = u'\N{QUOTATION MARK}'
 
 def contains_number(value):
     numbers = re.findall("[0-9]+", value)
     return True if numbers else False
 
-
 def colour(celsius):
     """Colourise temperatures"""
     c = float(celsius)
     if c < 0:
-        colour = "blue"
+        colour = 'blue'
     elif c == 0:
-        colour = "teal"
+        colour = 'teal'
     elif c < 10:
-        colour = "light blue"
+        colour = 'light blue'
     elif c < 20:
-        colour = "light green"
+        colour = 'light green'
     elif c < 30:
-        colour = "yellow"
+        colour = 'yellow'
     elif c < 40:
-        colour = "orange"
+        colour = 'orange'
     else:
-        colour = "red"
-    string = f"{c}{degree_sign}C"
+        colour = 'red'
+    string = (f'{c}{degree_sign}C')
 
     return ircutils.mircColor(string, colour)
 
+def day(lat, lon):
+    home      = ephem.Observer()
+    home.lat  = str(lat)           # str() Latitude
+    home.lon  = str(lon)           # str() Longitude
+
+    next_sunrise  = home.next_rising(ephem.Sun()).datetime()
+    next_sunset   = home.next_setting(ephem.Sun()).datetime()
+
+    day = ''
+    if next_sunset < next_sunrise:
+        day = True
+    else:
+        day = False
+    return day
 
 # XXX Converts decimal degrees to degrees, minutes, and seconds
 @lru_cache(maxsize=4)  # XXX LRU caching
@@ -106,183 +123,35 @@ def dd2dms(longitude, latitude):
 
     # account for E/W & N/S
     if degrees_x < 0:
-        EorW = "W"
+        EorW = 'W'
     else:
-        EorW = "E"
+        EorW = 'E'
 
     if degrees_y < 0:
-        NorS = "S"
+        NorS = 'S'
     else:
-        NorS = "N"
+        NorS = 'N'
 
     # abs() remove negative from degrees, was only needed for if-else above
     x = (
         str(abs(degrees_x))
-        + f"{degree_sign}"
+        + f'{degree_sign}'
         + str(minutes_x)
-        + f"{apostrophe} "
+        + f'{apostrophe} '
         + str(seconds_x)
-        + f"{quotation_mark} "
+        + f'{quotation_mark} '
         + EorW
     )
     y = (
         str(abs(degrees_y))
-        + f"{degree_sign}"
+        + f'{degree_sign}'
         + str(minutes_y)
-        + f"{apostrophe} "
+        + f'{apostrophe} '
         + str(seconds_y)
-        + f"{quotation_mark} "
+        + f'{quotation_mark} '
         + NorS
     )
     return (x, y)
-
-
-def format_uvi_icon(ico):
-    """
-    Diplays a coloured icon relevant to the UV Index meter.
-    Low: Green Moderate: Yellow High: Orange Very Hight: Red
-    Extreme: Violet 🥵
-    """
-    ico = float(ico)
-    if ico >= 0 and ico <= 2.9:
-        icon = "🟢"
-    elif ico >= 2 and ico <= 5.9:
-        icon = "🟡"
-    elif ico >= 5 and ico <= 7.9:
-        icon = "🟠"
-    elif ico >= 7 and ico <= 10.9:
-        icon = "🔴"
-    else:
-        icon = "🟣"
-    return icon
-
-
-def format_weather_output(response):
-    """
-    Gather all the data - format it
-    """
-    try:
-        location = response["location"]
-    except KeyError:
-        raise callbacks.Error("404: city not found")
-
-    current   = response["current"]
-
-    city_name = location["name"]
-    region    = location["region"]
-    country   = location["country"]
-
-    cr_date   = location["localtime"]
-    cr_date   = datetime.strptime(cr_date, "%Y-%m-%d %H:%M")
-    cr_date   = cr_date.strftime("%d-%m-%Y %H:%M")
-
-    # Convert lat, lon data into Degrees Minutes Seconds
-    (lon, lat)   = dd2dms(int(float(location["lon"])), int(float(location["lat"])))
-
-    description  = current["weather_descriptions"]
-    atmos        = current["pressure"]
-    weather_code = current["weather_code"]
-    # Get the cloud cover percentage
-    cloud        = current["cloudcover"]
-    # Calculate the direction of the positional arrows
-    arrow        = get_wind_direction(current["wind_degree"])
-    precip       = current["precip"]
-    humidity     = current["humidity"]
-    temp         = current["temperature"]
-    feelslike    = current["feelslike"]
-    wind         = current["wind_speed"]
-    uvi          = current["uv_index"]
-    utc          = location["utc_offset"]
-    visibility   = response["current"]["visibility"]
-    # Get weather_code from Weatherstack
-    status_icon  = get_status_icon(weather_code)
-    uvi_icon     = format_uvi_icon(uvi)
-
-    if precip:
-        precipico = "☔"
-    else:
-        precipico = ""
-
-    # Remove unwanted characters from 'weather_descriptions'
-    description = re.sub("[]'[]", "", str(description))
-
-    # Format output
-    a = f"🏠 {city_name} {region} {country} :: Lat {lat} Lon {lon} :: UTC {utc} :: {cr_date} :: {status_icon} {description} "
-    b = f"| 🌡 Barometric {atmos}hPa | ☁ Cloud cover {cloud}{percent_sign} | {precipico} Precip {precip}mm/h "
-    c = f"| 💦 Humidity {humidity}{percent_sign} | Current {colour(temp)} "
-    d = f"| Feels like {colour(feelslike)} | 🍃 Wind {wind}Km/H {arrow} "
-    e = f"| 👁 Visibility {visibility}Km | UVI {uvi} {uvi_icon}"
-
-    s = ""
-    seq = [a, b, c, d, e]
-    return s.join(seq)
-
-
-# Select the appropriate weather status icon
-def get_status_icon(code):
-    """Use the given code to attach appropriate
-    weather status icon"""
-    code = str(code)
-    switcher = {
-        "113": "☀",
-        "116": "🌤",
-        "119": "☁",
-        "122": "☁",
-        "143": "🌫",
-        "176": "🌧",
-        "248": "🌫",
-        "296": "🌧",
-        "302": "🌧",
-        "329": "❄",
-        "353": "🌧",
-        "356": "🌧",
-        "371": "❄",
-    }
-    return switcher.get(code, "🤷")
-
-
-def get_wind_direction(wind):
-    num = wind
-    val = int((num / 22.5) + 0.5)
-    # Wind direction decoration output
-    arr = [
-        "↑ N",
-        "NNE",
-        "↗ NE",
-        "ENE",
-        "→ E",
-        "ESE",
-        "↘ SE",
-        "SSE",
-        "↓ S",
-        "SSW",
-        "↙ SW",
-        "WSW",
-        "← W",
-        "WNW",
-        "↖ NW",
-        "NNW",
-    ]
-    return arr[(val % 16)]
-
-
-@lru_cache(maxsize=4)  # XXX LRU caching
-def query_postal_code(code):
-    """
-    This function returns longitude and latitude from
-    a postcode."""
-    postcode = code.split(",", 1)[0]
-    try:
-        countrycode = re.sub("[ ]", "", code.split(",", 1)[1])
-    except IndexError:
-        raise callbacks.Error("<postcode, country code>")
-    try:
-        nomi = pgeocode.Nominatim(countrycode)
-    except ValueError:
-        raise callbacks.Error(f"{countrycode} is not a known country code.")
-    zip = nomi.query_postal_code(postcode)
-    return [zip.latitude, zip.longitude]
-
 
 class Weatherstack(callbacks.Plugin):
     """
@@ -297,6 +166,91 @@ class Weatherstack(callbacks.Plugin):
         self.__parent = super(Weatherstack, self)
         self.__parent.__init__(irc)
 
+    def format_weather_output(self, response):
+        """
+        Gather all the data - format it
+        """
+        try:
+            location = response['location']
+        except KeyError:
+            raise callbacks.Error('404: city not found')
+
+        current   = response['current']
+
+        city_name = location['name']
+        region    = location['region']
+        country   = location['country']
+
+        cr_date   = location['localtime']
+        cr_date   = datetime.strptime(cr_date, '%Y-%m-%d %H:%M')
+        cr_date   = cr_date.strftime('%d-%m-%Y %H:%M')
+
+        # Convert lat, lon data into Degrees Minutes Seconds
+        (lon, lat)   = dd2dms(int(float(location['lon'])), int(float(location['lat'])))
+
+        description  = current['weather_descriptions']
+        atmos        = current['pressure']
+        weather_code = current['weather_code']
+        # Get the cloud cover percentage
+        cloud        = current['cloudcover']
+        # Calculate the direction of the positional arrows
+        arrow        = self._get_wind_direction(current['wind_degree'])
+        precip       = current['precip']
+        humidity     = current['humidity']
+        temp         = current['temperature']
+        feelslike    = current['feelslike']
+        wind         = current['wind_speed']
+        uvi          = current['uv_index']
+        utc          = location['utc_offset']
+        visibility   = response['current']['visibility']
+
+        uvi_icon     = self._format_uvi_icon(uvi)
+
+        # Get weather_code from Weatherstack
+        if not day(location['lat'], location['lon']):
+            status_icon = '🌚'
+        else:
+            status_icon = self._get_status_icon(weather_code)
+
+        if precip:
+            precipico = '☔'
+        else:
+            precipico = ''
+
+        # Remove unwanted characters from 'weather_descriptions'
+        description = re.sub('[]\'[]', '', str(description))
+
+        # Format output
+        a = f'🏠 {city_name} {region} {country} :: Lat {lat} Lon {lon} :: UTC {utc} :: {cr_date} :: {status_icon} {description} '
+        b = f'| 🌡 Barometric {atmos}hPa | ☁ Cloud cover {cloud}{percent_sign} | {precipico} Precip {precip}mm/h '
+        c = f'| 💦 Humidity {humidity}{percent_sign} | Current {colour(temp)} '
+        d = f'| Feels like {colour(feelslike)} | 🍃 Wind {wind}Km/H {arrow} '
+        e = f'| 👁 Visibility {visibility}Km | UVI {uvi} {uvi_icon}'
+
+        s = ""
+        seq = [a, b, c, d, e]
+        return s.join(seq)
+
+    @staticmethod
+    def _format_uvi_icon(uvi):
+        """
+        Diplays a coloured icon relevant to the UV Index meter.
+        Low: Green Moderate: Yellow High: Orange Very High: Red
+        Extreme: Violet 🥵
+        """
+        ico = float(uvi)
+        if ico >= 0 and ico <= 2.9:
+            icon = '🟢'
+        elif ico >= 2 and ico <= 5.9:
+            icon = '🟡'
+        elif ico >= 5 and ico <= 7.9:
+            icon = '🟠'
+        elif ico >= 7 and ico <= 10.9:
+            icon = '🔴'
+        else:
+            icon = '🟣'
+        return icon
+
     @lru_cache(maxsize=4)  # XXX LRU caching
     def get_location_by_location(self, latitude, longitude):
         """
@@ -306,23 +260,99 @@ class Weatherstack(callbacks.Plugin):
         # Missing API Key.
         if not apikey:
             raise callbacks.Error(
-                "Please configure the positionstack API key in config plugins.Weatherstack.positionstackAPI"
+                'Please configure the positionstack API key in config plugins.Weatherstack.positionstackAPI'
             )
-        coordinates = f"{latitude}, {longitude}"
-        params      = {"access_key": apikey, "query": coordinates, "limit": "1"}
+        coordinates = f'{latitude}, {longitude}'
+        params      = {'access_key': apikey, 'query': coordinates, 'limit': '1'}
 
-        r = requests.get("http://api.positionstack.com/v1/reverse", params)
+        r = requests.get('http://api.positionstack.com/v1/reverse', params)
         responses = r.json()
+
         try:
-            locality = responses["data"][0].get("locality")
+            locality = responses['data'][0].get('locality')
         except KeyError:
-            raise callbacks.Error("404: city not found")
+            raise callbacks.Error('404: city not found')
 
         self.log.info(
-            f"WeatherStack: get_location_by_location {locality}: {latitude},{longitude}"
+            f'WeatherStack: get_location_by_location {locality}: {latitude},{longitude}'
         )
 
         return locality
+
+    # Select the appropriate weather status icon
+    @staticmethod
+    def _get_status_icon(code):
+        """
+        Use the given code to attach appropriate
+        weather status icon
+        """
+        code = str(code)
+        switcher = {
+            '113': '☀',
+            '116': '🌤',
+            '119': '☁',
+            '122': '☁',
+            '143': '🌫',
+            '176': '🌧',
+            '248': '🌫',
+            '266': '🌦',
+            '293': '🌧',
+            '296': '🌧',
+            '302': '🌧',
+            '326': '🌨',
+            '329': '❄',
+            '353': '🌧',
+            '356': '🌧',
+            '371': '❄',
+            '389': '⛈'
+        }
+        return switcher.get(code, "🤷")
+
+    @staticmethod
+    def _get_wind_direction(degrees):
+        """Calculate wind direction"""
+        num = degrees
+        val = int((num/22.5)+.5)
+        # Decorated output
+        arr = [
+            '↑ N',
+            'NNE',
+            '↗ NE',
+            'ENE',
+            '→ E',
+            'ESE',
+            '↘ SE',
+            'SSE',
+            '↓ S',
+            'SSW',
+            '↙ SW',
+            'WSW',
+            '← W',
+            'WNW',
+            '↖ NW',
+            'NNW'
+        ]
+        return arr[(val % 16)]
+
+    @lru_cache(maxsize=4)  # XXX LRU caching
+    def query_postal_code(self, code):
+        """
+        This function returns longitude and latitude from
+        a postcode."""
+        postcode = code.split(',', 1)[0]
+        try:
+            countrycode = re.sub('[ ]', '', code.split(',', 1)[1])
+        except IndexError:
+            raise callbacks.Error('postcode, country code>')
+        try:
+            nomi = pgeocode.Nominatim(countrycode)
+        except ValueError:
+            raise callbacks.Error(f'{countrycode} is not a known country code.')
+        zip = nomi.query_postal_code(postcode)
+
+        self.log.info(f'Weatherstack: query_postal_code: {zip.latitude} {zip.longitude}')
+
+        return [zip.latitude, zip.longitude]
 
     @wrap(["text"])
     def weather(self, irc, msg, args, location):
@@ -335,43 +365,43 @@ class Weatherstack(callbacks.Plugin):
         """
         location = location.lower()
 
-        apikey = self.registryValue("weatherstackAPI")
+        apikey = self.registryValue('weatherstackAPI')
         # Missing API Key.
         if not apikey:
             raise callbacks.Error(
-                "Please configure the Weatherstack API key in config plugins.Weatherstack.weatherstackAPI"
+                'Please configure the Weatherstack API key in config plugins.Weatherstack.weatherstackAPI'
             )
 
         # Not 'enabled' in #channel.
-        if not self.registryValue("enable", msg.channel, irc.network):
+        if not self.registryValue('enable', msg.channel, irc.network):
             return
 
-        self.log.info(f"WeatherStack: running on {irc.network}/{msg.channel}")
+        self.log.info(f'WeatherStack: running on {irc.network}/{msg.channel}')
 
         # Check if 'location' is a postcode.
         if contains_number(location):
-            (lat, lon) = query_postal_code(location)
+            (lat, lon) = self.query_postal_code(location)
             location = self.get_location_by_location(lat, lon)
 
         # Initialise API data
-        params = {"access_key": apikey, "query": location, "units": "m"}
+        params = {'access_key': apikey, 'query': location, 'units': 'm'}
 
         try:
-            api_result = requests.get("http://api.weatherstack.com/current", params)
+            api_result = requests.get('http://api.weatherstack.com/current', params)
 
             # If the response was successful, no Exception will be raised
             api_result.raise_for_status()
         except HTTPError as http_err:
-            self.log.error(f"Weather: HTTP error occurred: {http_err}", exc_info=True)
-            raise callbacks.Error(f"Weather: HTTP error occurred: {http_err}")
+            self.log.error(f'Weather: HTTP error occurred: {http_err}', exc_info=True)
+            raise callbacks.Error(f'Weather: HTTP error occurred: {http_err}')
         except Exception as err:
-            self.log.error(f"Weather: an error occurred: {err}", exc_info=True)
-            raise callbacks.Error(f"Weather: an error occurred: {err}")
+            self.log.error(f'Weather: an error occurred: {err}', exc_info=True)
+            raise callbacks.Error(f'Weather: an error occurred: {err}')
         else:
             api_response = api_result.json()  # Data collection
 
             # Print the weather output
-            irc.reply(format_weather_output(api_response))
+            irc.reply(self.format_weather_output(api_response))
 
     @wrap(["something"])
     def help(self, irc):
